6a967a5b1e9e7b1ea6198888328788b1853e5102
Fars MD <fars@japranmceh.com>
2026-07-04 04:29:39 +0700

    add: custom exception handler for API JSON responses

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -7,6 +7,10 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    protected $dontReport = [
+        //
+    ];
+
     protected $dontFlash = [
         'current_password',
         'password',
@@ -18,5 +22,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+                'exception' => class_basename($exception),
+            ], 500);
+        }
+
+        return parent::render($request, $exception);
     }
 }
